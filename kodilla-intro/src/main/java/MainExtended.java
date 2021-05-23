1509c7423ea989c4a8bc7fb204a551d82c354186
Paweł Budych <pawel.budych@gmail.com>
2021-05-23 16:21:04 +0200

    zadanie drugie czyli zmienne

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String car = "FSO Polonez Caro";
+        int manufactured = 1995;
+        double displacement = 1.6;
+        String equipment = "GLI";
+        char drivingLicense = 'A';
+        boolean isItQuick = false;
+        System.out.println(car);
+        System.out.println(manufactured);
+        System.out.println(displacement);
+        System.out.println(equipment);
+        System.out.println(drivingLicense);
+        System.out.println(isItQuick);
+    }
+}
