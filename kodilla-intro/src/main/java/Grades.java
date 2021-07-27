082b996c464a486c25771ef99000c136e2739398
Paweł Budych <pawel.budych@gmail.com>
2021-07-27 23:08:19 +0200

    zadanie 2.4

---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,37 @@
+public class Grades {
+    private int[] grades;
+    private int size;
+
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+
+        public void add(int value) {
+            if (this.size == 10) {
+                return;
+            }
+            this.grades[this.size] = value;
+            this.size++;
+        }
+
+        public int latestGrade() {
+            int grade = grades[size];
+            return grade;
+        }
+
+        public int average() {
+
+            int sum = 0;
+            for (int i = 0; i < grades.length; i++) {
+                sum = sum + grades[i];
+            }
+
+
+            int average = 0;
+            average = sum / size;
+            return average;
+        }
+}
+
