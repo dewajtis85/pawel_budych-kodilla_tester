205cf9d90301209da53fc4f724e4159053bb9194
Paweł Budych <pawel.budych@gmail.com>
2021-09-05 19:51:33 +0200

    zadanie 2.6

---
+++ kodilla-intro/src/main/java/Application.java
@@ -0,0 +1,7 @@
+public class Application {
+    public static void main(String[] args) {
+            User2 adam = new User2("Adam", 40.5, 178);
+            adam.describeUser();
+        }
+    }
+
